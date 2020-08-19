36847ec9d1d19d24a7bcd98145021d4cafb89c83
youxingxiang <1365831278@qq.com>
2020-08-19 12:09:51 +0800

    第一次提交

---
+++ README.md
@@ -0,0 +1,1 @@
+<p align="center"><code>Quick Admin</code></p>
---
+++ config/admin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: youxingxiang
+ * Date: 2020/8/19
+ * Time: 12:09 PM
+ */
+return [
+
+];
